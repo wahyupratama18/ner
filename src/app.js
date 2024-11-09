22601eb4f4e82bde45f38fc6710c288942e4be16
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2024-11-09 19:43:25 +0700

    finalize view in index and app, also insert charts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import './app.css';
 import Alpine from 'alpinejs';
 import axios from 'axios';
+import { Chart, registerables } from 'chart.js';
 
 window.axios = axios;
 window.Alpine = Alpine;
+window.Chart = Chart;
+
+Chart.register(...registerables);
 
 Alpine.start();
